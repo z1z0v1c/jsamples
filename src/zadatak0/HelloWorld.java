898ee9671e910bc50d154214a017dec0ee811743
Aleksandar Zizovic <Aleksandar.Zizovic@endava.com>
2018-09-12 14:59:50 +0200

    Dodat tekst zadatka i autor u zadatak 0

--- src/zadatak0/HelloWorld.java
+++ src/zadatak0/HelloWorld.java
@@ -1,6 +1,13 @@
+/**
+ * Kreirati projekat u razvojnom okruzenju (Eclipse, IntelliJ) i napisati program koji u konzoli ispisuje
+ * tekst: "Hello World!".
+ */
 package zadatak0;
 
-
+/**
+ * @author Aleksandar.Zizovic
+ *
+ */
 public class HelloWorld {
  public static void main(String [] args) {
 	 System.out.print("Hello World!");
