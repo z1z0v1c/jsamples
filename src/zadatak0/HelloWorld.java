4f01b3e35b652a5b84ec7e4dd04cd2f7bb29ef9c
Aleksandar Zizovic <aleksandar.zizovic@endava.com>
2018-09-13 09:56:39 +0200

    Autor i tekst zadatka spojeni u jedan javadoc

--- src/zadatak0/HelloWorld.java
+++ src/zadatak0/HelloWorld.java
@@ -1,15 +1,16 @@
-/**
- * Kreirati projekat u razvojnom okruzenju (Eclipse, IntelliJ) i napisati program koji u konzoli ispisuje
- * tekst: "Hello World!".
- */
+
 package zadatak0;
 
 /**
+ * Kreirati projekat u razvojnom okruzenju (Eclipse, IntelliJ) i napisati program koji u konzoli ispisuje
+ * tekst: "Hello World!".
+ * 
  * @author Aleksandar.Zizovic
  *
  */
-public class HelloWorld {
- public static void main(String [] args) {
+
+public class HelloWorld {	
+ public static void main(String [] args) {	 
 	 System.out.print("Hello World!");
  }
 }
