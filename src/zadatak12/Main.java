ec856d7543da3b78938f3f5ec623b7ea6a9363f9
Aleksandar Zizovic <Aleksandar.Zizovic@endava.com>
2018-09-14 11:41:13 +0200

    Dodat zadatak 12

--- src/zadatak12/Main.java
+++ src/zadatak12/Main.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package zadatak12;
 
 import java.io.File;
@@ -10,50 +8,96 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-/**
+/** 
+ * Napisati program koji umogucava unos imena studenta i broj poena osvojenih na testu preko
+ * standardnog ulaza (sve dok se ne unese odredjen znak za prekid unosa podataka).
+ * Broj poena sme da bude od 0 do 100.
+ * Kao r4ezultat programa,  ocekuje se izlazni fiajl "ocene.txtx" u kome ce se nalaziti lista 
+ * svih studenata sortirana po poenima, od najvise ka najmanje, i gde ce se osim imena studenta
+ * i boja poena nalaziti i ocena koju je dobio na ispiti.
+ * Ocena 10 akoje imao vise od 90 poena, 9 ako je imao izmedju 81 i 90... Sve do 5 ako je imao 
+ * manje od 51 poena.  
+ * 
  * @author Aleksandar.Zizovic
  *
  */
 public class Main {
 
 	private static Scanner ulaz = new Scanner(System.in);
-	private static final String IME = "Unesite ime studenata (0 za izlaz iz programa): ";
-	private static final String PREZIME = "Student sa najvecim brojem poena: ";
+	private static final String IME = "Unesite ime studenata (X za izlaz iz programa): ";
+	private static final String PREZIME = "Unesite prezime studenata: ";
 	private static final String BR_POENA = "Broj poena koji je student ostvario: ";
 	private static final String POGRESAN_UNOS = "Pogresan unos! Pokusajte ponovo!";
+	private static final String REGEX = "[a-zA-Z]+";
 
 	public static void main(String[] args) {
 
 		ArrayList<Student> studenti = new ArrayList<>();
 		boolean nastavi = true;
 
+		//unos studenata
 		while (nastavi) {
 			System.out.println(IME);
-			String ime = ulaz.next();
-			if (ime.equals("0"))
+			String ime = unosImenaiPrezimena();
+			if (ime.equalsIgnoreCase("x"))
 				break;
 			Student student = new Student();
 			student.setIme(ime);
 			System.out.println(PREZIME);
-			student.setPrezime(ulaz.next());
+			student.setPrezime(unosImenaiPrezimena());
 			System.out.println(BR_POENA);
-			student.setBrojPoena(ulaz.nextInt());
+			student.setBrojPoena(unosBrojaPoena());
 			studenti.add(student);
 		}
 
 		Collections.sort(studenti);
+		Collections.reverse(studenti);
 
 		File fajl = new File("ocena.txt");
 
-		for (Student s : studenti) {
-			try {
-				PrintWriter izlaz = new PrintWriter(fajl);
+		//upisivanje studenata u fajl
+		try {
+			PrintWriter izlaz = new PrintWriter(fajl);
+			for (Student s : studenti) {
 				izlaz.print(s.toString());
-				izlaz.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+				izlaz.println("");
 			}
+			izlaz.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 		ulaz.close();
 	}
+	
+	private static String unosImenaiPrezimena() {
+		String s = "";
+		while (!s.matches(REGEX)) {
+			if (!s.equals("")) {
+				System.out.println(POGRESAN_UNOS);
+			}
+			s = ulaz.nextLine();
+		}
+		return s;
+	}
+	
+	//provera da li je unet broj
+		private static int unosBroja() {
+			while (!ulaz.hasNextInt()) {
+				System.out.println(POGRESAN_UNOS);
+				ulaz.next();
+			}
+			return ulaz.nextInt();
+		}
+	
+	//validacija unetog broja studenata
+	private static int unosBrojaPoena() {
+		int broj = unosBroja();
+		while(broj < 0) {
+			System.out.println(POGRESAN_UNOS);
+			broj = unosBroja();
+		} 
+			return broj;
+	}
+	
+	
 }
--- src/zadatak12/Student.java
+++ src/zadatak12/Student.java
@@ -75,6 +75,6 @@ public class Student implements Comparable<Student> {
 
 	@Override
 	public String toString() {
-		return new StringBuilder(this.ime).append(" ").append(this.prezime).append(" ").append(this.brojPoena).append(" ").append(this.getOcena()).append("\n").toString();
+		return new StringBuilder(this.ime).append(" ").append(this.prezime).append(" ").append(this.brojPoena).append(" ").append(this.getOcena()).append(" \n").toString();
 	}
 }
