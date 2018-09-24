9396a78d6b607c2984908065b87e2feb9a235b25
Aleksandar Zizovic <Aleksandar.Zizovic@endava.com>
2018-09-24 16:54:33 +0200

    Ispravljene 2 stavke

--- src/zadatak12/Main.java
+++ src/zadatak12/Main.java
@@ -9,15 +9,15 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-/** 
- * Napisati program koji umogucava unos imena studenta i broj poena osvojenih na testu preko
- * standardnog ulaza (sve dok se ne unese odredjen znak za prekid unosa podataka).
- * Broj poena sme da bude od 0 do 100.
- * Kao rezultat programa,  ocekuje se izlazni fajl "ocene.txt" u kome ce se nalaziti lista 
- * svih studenata sortirana po poenima, od najvise ka najmanje, i gde ce se osim imena studenta
- * i boja poena nalaziti i ocena koju je dobio na ispiti.
- * Ocena 10 akoje imao vise od 90 poena, 9 ako je imao izmedju 81 i 90... Sve do 5 ako je imao 
- * manje od 51 poena.  
+/**
+ * Napisati program koji umogucava unos imena studenta i broj poena osvojenih na
+ * testu preko standardnog ulaza (sve dok se ne unese odredjen znak za prekid
+ * unosa podataka). Broj poena sme da bude od 0 do 100. Kao rezultat programa,
+ * ocekuje se izlazni fajl "ocene.txt" u kome ce se nalaziti lista svih
+ * studenata sortirana po poenima, od najvise ka najmanje, i gde ce se osim
+ * imena studenta i boja poena nalaziti i ocena koju je dobio na ispiti. Ocena
+ * 10 akoje imao vise od 90 poena, 9 ako je imao izmedju 81 i 90... Sve do 5 ako
+ * je imao manje od 51 poena.
  * 
  * @author Aleksandar.Zizovic
  *
@@ -29,7 +29,7 @@ public class Main {
 	private static final String PREZIME = "Unesite prezime studenata: ";
 	private static final String BR_POENA = "Broj poena koji je student ostvario: ";
 	private static final String POGRESAN_UNOS = "Pogresan unos! Pokusajte ponovo!";
-	private static final String REGEX = "[a-zA-Z]+";
+	private static final String REGEX = "^[a-zA-Z]+[\\-'\\s]?[a-zA-Z ]+$";
 	private static final String NASTAVAK = "Da li zelite da nastavite (da/ne)?";
 	private static final String KRAJ = "Kraj programa!";
 
@@ -38,7 +38,7 @@ public class Main {
 		List<Student> studenti = new ArrayList<>();
 		boolean nastavi = true;
 
-		//unos studenata
+		// unos studenata
 		while (nastavi) {
 			System.out.println(IME);
 			String ime = unosImenaiPrezimena();
@@ -49,8 +49,8 @@ public class Main {
 			System.out.println(BR_POENA);
 			student.setBrojPoena(unosBrojaPoena());
 			studenti.add(student);
-			
-			//nastavak ili izlaz iz programa
+
+			// nastavak ili izlaz iz programa
 			System.out.println(NASTAVAK);
 			if (!nastavak().equalsIgnoreCase("da")) {
 				System.out.println(KRAJ);
@@ -63,20 +63,18 @@ public class Main {
 
 		File fajl = new File("ocena.txt");
 
-		//upisivanje studenata u fajl
-		try {
-			PrintWriter izlaz = new PrintWriter(fajl);
+		// upisivanje studenata u fajl
+		try (PrintWriter izlaz = new PrintWriter(fajl)) {
 			for (Student student : studenti) {
 				izlaz.print(student.toString());
 				izlaz.println("");
 			}
-			izlaz.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		ulaz.close();
 	}
-	
+
 	private static String unosImenaiPrezimena() {
 		String imeIPrezime = "";
 		while (!imeIPrezime.matches(REGEX)) {
@@ -87,33 +85,33 @@ public class Main {
 		}
 		return imeIPrezime;
 	}
-	
-	//provera da li je unet broj
-		private static int unosBroja() {
-			while (!ulaz.hasNextInt()) {
-				System.out.println(POGRESAN_UNOS);
-				ulaz.next();
-			}
-			return ulaz.nextInt();
+
+	// provera da li je unet broj
+	private static int unosBroja() {
+		while (!ulaz.hasNextInt()) {
+			System.out.println(POGRESAN_UNOS);
+			ulaz.next();
 		}
-	
-	//validacija unetog broja studenata
+		return ulaz.nextInt();
+	}
+
+	// validacija unetog broja studenata
 	private static int unosBrojaPoena() {
 		int broj = unosBroja();
-		while(broj < 0) {
+		while (broj < 0) {
 			System.out.println(POGRESAN_UNOS);
 			broj = unosBroja();
-		} 
-			return broj;
-	}
-	
-	//validacija odgovora za nastavak ili izlaz iz programa
-		private static String nastavak() {
-			String nastavak = ulaz.next();
-			while (!(nastavak.equalsIgnoreCase("da") || nastavak.equalsIgnoreCase("ne"))) {
-				System.out.println(POGRESAN_UNOS);
-				 nastavak = ulaz.next();
-			}
-			return nastavak;	 		
 		}
+		return broj;
+	}
+
+	// validacija odgovora za nastavak ili izlaz iz programa
+	private static String nastavak() {
+		String nastavak = ulaz.next();
+		while (!(nastavak.equalsIgnoreCase("da") || nastavak.equalsIgnoreCase("ne"))) {
+			System.out.println(POGRESAN_UNOS);
+			nastavak = ulaz.next();
+		}
+		return nastavak;
+	}
 }
