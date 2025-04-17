1c136b79a1f4bca655afc3bef22bafe4871b6a0b
Aleksandar Zizovic <42997751+z1z0v1c@users.noreply.github.com>
2025-04-17 03:19:41 +0200

    Update doc comment for real estate task

--- src/pckg09realestate/Main.java
+++ src/pckg09realestate/Main.java
@@ -3,20 +3,20 @@ package pckg09realestate;
 /**
  * Create the Real Estate project in the following way: Abstract Real Estate class that has the following attributes:
  * <p>
- * 	- Address, zone, square footage and information about to the owner/instance of the Owner class.
- *  - The owner has full name and surname, ID, and card number.
- *  - The derived classes, Apartment and House have a toString() method where all information about the property,
- *  including the Owner and the price, is printed.
- *  - The price is calculated based on the square footage of X zone (I zone 3000, II zone 2000, III zone 1000, IV zone
- *  500e).
- *  - The house has an additional area of the yard.
- *  - The price of the house includes the price of the yard, which is calculated according to the principle (yard area X
- *  Zone X 15%).
- *  - The apartment has two additional fields, the Basement area and the Terrace area.
- *  - The price of the apartment includes the price of the basement and terrace ((basement area + terrace area) X Zone X
- *  33%).
+ *	- Address, zone, square footage, and information about the owner/instance of the Owner class.
+ *  	- The owner has a full first and last name, ID, and card number.
+ *  	- The derived classes, Apartment and House, have a toString() method where all information about the property,
+ *  	including the Owner and the price, is printed.
+ *  	- The price is calculated based on the square footage of X zone (I zone 3000, II zone 2000, III zone 1000, IV zone
+ *  	500e).
+ *  	- The house has an additional yard area.
+ *  	- The price of the house includes the price of the yard, which is calculated according to the principle (yard area X
+ *  	Zone X 15%).
+ *  	- The apartment has two additional fields, the Basement area and the Terrace area.
+ *  	- The price of the apartment includes the price of the basement and terrace ((basement area + terrace area) X Zone X
+ *  	33%).
  * <p>
- * Create the Main class and in it create instances of the Property class and create objects of derived classes with
+ * Create the Main class and create instances of the Property class in it. Create objects of derived classes with
  * arbitrary arguments. Print String representation of objects - toString().
  *
  * @author Aleksandar Zizovic
